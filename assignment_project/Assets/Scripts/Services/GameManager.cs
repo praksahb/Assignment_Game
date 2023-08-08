3f164a68b426de5f64b0f13d8a087e433b943fc9
praksahb <bhaskar1126@gmail.com>
2023-08-08 19:19:36 +0530

    minor changes
    - changed file name GameLoop to GameManager
    
    - position.x can now be changed using tileposition values
    
    - move position added for moving on x axis direction only

---
+++ assignment_project/Assets/Scripts/Services/GameManager.cs
@@ -0,0 +1,58 @@
+using TileGame.Player;
+using TileGame.Tiles;
+using UnityEngine;
+
+namespace TileGame.MainGame
+{
+    // trying it without generic singleton for modularity
+
+    public class GameManager : MonoBehaviour
+    {
+        [SerializeField]
+        private PlayerView playerRedPrefab;
+        [SerializeField]
+        private PlayerView playerBluePrefab;
+        [SerializeField]
+        private TileView tilePrefab;
+        [SerializeField]
+        private float distanceBetweenTiles = 1.1f;
+        [SerializeField]
+        private int totalTiles = 10;
+
+        // game obj private references
+        private PlayerController playerRed;
+        private PlayerController playerBlue;
+        private TileListController tileListController;
+
+
+        // pvt variables to be reused
+        private Vector3 currentTilePosition;
+        private int playerRedPosition = 0;
+        private int playerBluePosition;
+
+        private void Awake()
+        {
+            playerBluePosition = totalTiles - 1;
+            InitializeGame();
+        }
+
+        private void InitializeGame()
+        {
+            // instantiate tiles
+            tileListController = new TileListController(totalTiles, tilePrefab, distanceBetweenTiles);
+
+
+            // instantiate player objs
+
+            // red is on left start
+            PlayerModel playerRedModel = new PlayerModel(PlayerType.Red, playerRedPosition);
+            currentTilePosition.x = tileListController.GetTilePositionX(playerRedModel.TilePosition);
+            playerRed = new PlayerController(playerRedModel, playerRedPrefab, currentTilePosition.x);
+
+            // blue is on right end
+            PlayerModel playerBlueModel = new PlayerModel(PlayerType.Blue, playerBluePosition);
+            currentTilePosition.x = tileListController.GetTilePositionX(playerBlueModel.TilePosition);
+            playerBlue = new PlayerController(playerBlueModel, playerBluePrefab, currentTilePosition.x);
+        }
+    }
+}
--- assignment_project/Assets/Scripts/Tiles/TileList/TileListController.cs
+++ assignment_project/Assets/Scripts/Tiles/TileList/TileListController.cs
@@ -1,17 +1,41 @@
-﻿namespace TileGame.Tiles
+﻿using System;
+using UnityEngine;
+
+namespace TileGame.Tiles
 {
     public class TileListController
     {
         private TileController[] tileList;
+        private TileView tilePrefab;
 
-        public TileListController(int length, TileView tilePrefab)
+
+        public TileListController(int length, TileView tilePrefab, float distanceBetweenTiles)
         {
             tileList = new TileController[length];
+            this.tilePrefab = tilePrefab;
 
+            CreateTilesHorizontal(length, distanceBetweenTiles);
+        }
+
+        public float GetTilePositionX(int index)
+        {
+            if (index < 0 || index > tileList.Length)
+            {
+                throw new Exception();
+            }
+
+            return tileList[index].TileView.transform.position.x;
+        }
+
+
+
+        private void CreateTilesHorizontal(int length, float distanceBetweenTiles)
+        {
             for (int i = 0; i < length; i++)
             {
                 TileModel tileModel = new TileModel(i);
-                TileController tileController = new TileController(tileModel, tilePrefab);
+                Vector3 tilePosition = new Vector3(tileModel.TileNumber * distanceBetweenTiles, 0, 0);
+                TileController tileController = new TileController(tileModel, tilePrefab, tilePosition);
                 tileList[i] = tileController;
             }
         }
